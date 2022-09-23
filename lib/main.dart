31f273332099fb10cebcfbf8a668aed7531d8820
Alan Melo <amelo967@gmail.com>
2022-09-23 10:37:18 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:controle_carteiras/presentation/openMonth.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Bem vindo Alan!'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Row(
+          children: [
+            Container(
+              width: 60,
+              color: Colors.redAccent,
+              child: Column(),
+            ),
+            Expanded(child: Container(child: OpenMonth()))
+          ],
+        ),
+      ),
+    );
+  }
+}
